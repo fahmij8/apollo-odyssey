1715fc22f501d42fb36be0c4fa5ea1f3d18af150
Fahmi Jabbar <rootmawar77@gmail.com>
2022-08-18 18:13:53 +0700

    refactor(server): convert import statement to commonjs

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,4 +1,4 @@
-import { gql } from 'apollo-server';
+const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
@@ -24,4 +24,4 @@ const typeDefs = gql`
   }
 `;
 
-export default typeDefs;
+module.exports = typeDefs;
