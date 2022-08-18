6f683f75c9ef94ac108390d1dd027baa9bf06c5a
Fahmi Jabbar <rootmawar77@gmail.com>
2022-08-18 15:42:00 +0700

    feat(server): build schema

---
+++ server/src/schema.js
@@ -0,0 +1,27 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
+
+export default typeDefs;
