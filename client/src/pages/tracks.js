0da710f787d0b58a4c3a3a2c12ebb9cc7d3c04c3
Fahmi Jabbar <rootmawar77@gmail.com>
2022-08-22 11:13:51 +0700

    feat(client): implement useQuery hook to execute gql query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { Layout } from '../components';
+import { useQuery, gql } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult loading={loading} error={error} data={data}>
+        {data?.tracksForHome?.map((track, index) => (
+          <TrackCard key={`${track.id}_${index}`} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
