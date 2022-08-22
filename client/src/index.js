91df7bce03ff79ac265353d8d151ed67c176ba7b
Fahmi Jabbar <rootmawar77@gmail.com>
2022-08-22 10:58:58 +0700

    feat(client): setup apollo-graphql client

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
