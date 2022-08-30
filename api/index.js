9f82abdb63cad478cf199345e6cf3ebbcfd77538
Fahmi Jabbar <fahmijabbar12@gmail.com>
2022-08-31 00:07:08 +0700

    refactor(server): switch to apollo-server-express, comply to vercel serverless folder structure

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const express = require('express');
+const http = require('http');
+const typeDefs = require('./utils/schema');
+const resolvers = require('./utils/resolvers');
+const TrackAPI = require('./datasources/track-api');
+
+const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    };
+  },
+});
+
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`🚀 Server path is at ${server.graphqlPath}`);
+}
+
+startApolloServer();
+
+module.exports = httpServer;
