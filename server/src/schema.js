80625bc9500c95849cd8b31d12ee37aa299a4109
Fahmi Jabbar <fahmijabbar12@gmail.com>
2022-08-30 13:40:05 +0700

    feat(server): add query for specific module + additional field

--- server/src/schema.js
+++ server/src/schema.js
@@ -6,6 +6,8 @@ const typeDefs = gql`
     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
     track(id: ID!): Track!
+    "Fetch specific module"
+    module(id: ID!): Module!
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -45,6 +47,10 @@ const typeDefs = gql`
     title: String!
     "The module's length in minutes"
     length: Int
+    "The module's content"
+    content: String
+    "The modules's video URL"
+    videoUrl: String
   }
 `;
 
