227d40d3e12ec5194c66cae4d6c57e8aec874130
Fahmi Jabbar <fahmijabbar12@gmail.com>
2022-08-31 00:33:02 +0700

    fix(client): edit apollo server endpoint

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://catstronout-server-fahmi.vercel.app/', // change to YOUR own production server
+  uri: 'https://catstronout-server-fahmi.vercel.app/graphql', // change to YOUR own production server
   cache: new InMemoryCache(),
   name: 'web',
   version: '1.0',
